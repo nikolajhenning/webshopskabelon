507b72ab110df8cacc054df8917395048041bd7b
Hans Peter Gammeltoft <hpg@itstack.dk>
2022-04-29 14:10:14 +0200

    update shipping icons conditions

--- wp-content/themes/storefront-child-theme-master/inc/woocommerce/delivery.php
+++ wp-content/themes/storefront-child-theme-master/inc/woocommerce/delivery.php
@@ -39,7 +39,57 @@ function wooelements_filter_shipping_methods( $rates, $package ) {
 	return $rates;
 }
 
-
+/* Insert image before label on shipping method  */
+function filter_woocommerce_cart_shipping_method_full_label( $label, $method ) {
+    // Remove afterwards
+    //echo 'DEBUG: method id = '. $method->label;
+    // Use the condition here with $method to apply the image to a specific method.    
+    if ( $method->instance_id == 1 ) {
+      $label = "<i class='fas fa-truck'></i>" . $label;
+    }
+    elseif ( $method->instance_id == 2 ) {
+      $label = "<i class='fas fa-store-alt'></i>" . $label;
+    }
+    elseif ( $method->instance_id == 3 ) {
+      $gls_image_url = get_stylesheet_directory_uri() . '/img/gls-logo.svg';
+      $label = "<img class='shipping_logo' src='" . $gls_image_url . " ' " . "alt='GLS logo' />" . $label;
+    }
+    elseif ( $method->instance_id == 4 ) {
+      $gls_image_url = get_stylesheet_directory_uri() . '/img/gls-logo.svg';
+      $label = "<img class='shipping_logo' src='" . $gls_image_url . " ' " . "alt='GLS logo' />" . $label;
+    }
+    elseif ( $method->instance_id == 5 ) {
+      $gls_image_url = get_stylesheet_directory_uri() . '/img/gls-logo.svg';
+      $label = "<img class='shipping_logo' src='" . $gls_image_url . " ' " . "alt='GLS logo' />" . $label;
+    }
+    elseif ( $method->instance_id == 6 ) {
+      $gls_image_url = get_stylesheet_directory_uri() . '/img/postnord-logo.svg';
+      $label = "<img class='shipping_logo' src='" . $gls_image_url . " ' " . "alt='PostNord logo' />" . $label;
+    }
+    elseif ( $method->instance_id == 7 ) {
+      $gls_image_url = get_stylesheet_directory_uri() . '/img/postnord-logo.svg';
+      $label = "<img class='shipping_logo' src='" . $gls_image_url . " ' " . "alt='PostNord logo' />" . $label;
+    }
+    elseif ( $method->instance_id == 10 ) {
+      $gls_image_url = get_stylesheet_directory_uri() . '/img/postnord-logo.svg';
+      $label = "<img class='shipping_logo' src='" . $gls_image_url . " ' " . "alt='PostNord logo' />" . $label;
+    }
+    elseif ( $method->instance_id == 8 ) {
+      $gls_image_url = get_stylesheet_directory_uri() . '/img/dao-logo.svg';
+      $label = "<img class='shipping_logo' src='" . $gls_image_url . " ' " . "alt='DAO logo' />" . $label;
+    }
+    elseif ( $method->instance_id == 9 ) {
+      $gls_image_url = get_stylesheet_directory_uri() . '/img/dao-logo.svg';
+      $label = "<img class='shipping_logo' src='" . $gls_image_url . " ' " . "alt='DAO logo' />" . $label;
+    }
+    else {
+      $label = "<i class='fas fa-truck'></i>" . $label;
+    }
+    
+    return $label; 
+  }
+  add_filter( 'woocommerce_cart_shipping_method_full_label', 'filter_woocommerce_cart_shipping_method_full_label', 10, 2 ); 
+  
 
 /* Hide shipping based on category */
 function product_category_hide_shipping_methods( $rates, $package ){
