023408fb3b2d89a6de00bb790319c6c7b6427bd4
Hans Peter Gammeltoft <hpg@itstack.dk>
2022-05-16 11:38:35 +0200

    show ekstra category description fix

--- wp-content/themes/storefront-child-theme-master/woocommerce/archive-product.php
+++ wp-content/themes/storefront-child-theme-master/woocommerce/archive-product.php
@@ -117,12 +117,13 @@ if ( woocommerce_product_loop() ) {
 * show ekstra category description
 *
 */
-if ( is_product_taxonomy() ) {
-	$term = get_queried_object();
-	if ( $term && ! empty( get_woocommerce_term_meta( $term->term_id, 'seconddesc', true ) ) ) {
-	   echo '<p class="term-description">' . wc_format_content( htmlspecialchars_decode( get_woocommerce_term_meta( $term->term_id, 'seconddesc', true ) ) ) . '</p>';
-	}
- }
+if ( is_product_taxonomy() ) { 	
+	$term = get_queried_object(); 	
+	$term_meta = get_term_meta($term->term_id, 'seconddesc', true); 	
+	if ( $term && ! empty( $term_meta ) ) { 	   
+		echo '<p class="term-description">' . wc_format_content( htmlspecialchars_decode( $term_meta  )) . '</p>'; 	
+	}  
+}
 
 /**
  * Hook: woocommerce_after_main_content.
