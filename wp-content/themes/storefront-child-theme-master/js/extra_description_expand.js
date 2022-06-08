a2bd4cd75c912fa2e44ea783f7141a4b48dad97c
Hans Peter Gammeltoft <hpg@itstack.dk>
2022-06-08 10:23:07 +0200

    Expand text split for long term description

--- wp-content/themes/storefront-child-theme-master/js/extra_description_expand.js
+++ wp-content/themes/storefront-child-theme-master/js/extra_description_expand.js
@@ -1,30 +1,28 @@
 jQuery( function( $ ) {
-    $(document).ready(function() {
-
-        //Get current height of the description container
-        let currentHeight = $(".long-term-description").height();
-        if (currentHeight > 150 ) {
-
-            //Add "non-expand" class
-            $( ".long-term-description" ).addClass( "non-expand" );
-
-            //Add 'Read more' button
-            $(".long-term-description").after( "<a class='read-button read-more'>Læs mere</a>" );
-
-            //Set description container back to its original height
-            $( ".read-button" ).on( "click", function(evt) {
-                evt.preventDefault();
-                $(".long-term-description").toggleClass('expand non-expand');
-                $(".expand").css({
-                    "max-height": currentHeight,
-                });
-                $(".non-expand").css({
-                    "max-height": "100px",
-                });
-                var button_text = $(".long-term-description").hasClass('expand') ? 'Læs mindre' : 'Læs mere';
-                $(".read-button").text(button_text);
-            });
-        }
+    var s = $(".long-term-description").html();
+    var split_lenght = 150;
+    
+    if (s.split(' ').length > split_lenght) {
+    
+        //Add "non-expand" class
+        $( ".long-term-description" ).addClass( "non-expand" );
+      
+        //Read more button
+        $(".long-term-description").after( "<a class='read-button'>Læs mere</a>" );
+    
+        //Text split
+        $(".long-term-description").html(s.split(' ').slice(0,split_lenght).join(' ') + " ... ");
+      
+      //On click toggle between full html and splittet html
+         $( ".read-button" ).on( "click", function() {
+             $(".long-term-description").toggleClass('non-expand expand');
+            
+        var button_text = $(".long-term-description").hasClass('expand') ? 'Læs mindre' : 'Læs mere';
+        $(".read-button").text(button_text);
+    
+            $(".long-term-description").hasClass('non-expand') ? $(".long-term-description").html(s.split(' ').slice(0,split_lenght).join(' ') + " ... ") : $(".long-term-description").html(s);
+                    
+                    
     });
+    }
 });
-
